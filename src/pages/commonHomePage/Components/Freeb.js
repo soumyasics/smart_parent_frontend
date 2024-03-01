9a26cde86a4f963f9dac30cce6070d9820071ff4
Anand R P <anandrp.sics@gmail.com>
2024-03-02 02:05:35 +0530

    home page content added

--- src/pages/commonHomePage/Components/Freeb.js
+++ src/pages/commonHomePage/Components/Freeb.js
@@ -1,45 +1,102 @@
-import React from 'react'
-import './Freeb.js'
+import React from "react";
+import "./Freeb.js";
 function Freeb() {
   return (
     <>
-    <div className='fb mb-5'>
-         <p><b>Free Blogs</b></p>
+      <div className="fb mb-5 ms-5">
+        <p style={{ fontSize: "35px" }}>
+          <b>Free Blogs To Read</b>
+        </p>
       </div>
-      <div className='free-container container'>
-      <div className='free-row' style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-      <div class="card" style={{ width: '18rem', transition: 'transform .9s', marginLeft:'15px' }}>
-  <div class="card-body">
-    <h5 class="card-title">Blog title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Blog subtitle</h6>
-    <p class="card-text">Blog content along with author name</p>
-  </div>
-</div>
-<div class="card" style={{ width: '18rem', transition: 'transform .9s'}}>
-  <div class="card-body">
-    <h5 class="card-title">Blog title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Blog subtitle</h6>
-    <p class="card-text">Blog content along with author name</p>
-  </div>
-</div>
-<div class="card" style={{ width: '18rem', transition: 'transform .9s'}}>
-  <div class="card-body">
-    <h5 class="card-title">Blog title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Blog subtitle</h6>
-    <p class="card-text">Blog content along with author name</p>
-  </div>
-</div>
-<div class="card" style={{ width: '18rem', transition: 'transform .9s',marginRight:'15px'}}>
-  <div class="card-body">
-    <h5 class="card-title">Blog title</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Blog subtitle</h6>
-    <p class="card-text">Blog content along with author name</p>
-  </div>
-</div>
+      <div className="free-container container">
+        <div
+          className="free-row"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexWrap: "wrap",
+          }}
+        >
+          <div
+            class="card"
+            style={{
+              width: "18rem",
+              transition: "transform .9s",
+              marginLeft: "15px",
+            }}
+          >
+            <img
+              src="https://www.himama.com/blog/wp-content/uploads/2018/12/header_kid_ipad.jpg"
+              alt="child"
+              style={{ height: "200px" }}
+            />
+            <div class="card-body">
+              <h5 class="card-title">Blog title</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">
+                Blog subtitle
+              </h6>
+              <p class="card-text">Blog content along with author name</p>
+            </div>
+          </div>
+          <div
+            class="card"
+            style={{ width: "18rem", transition: "transform .9s" }}
+          >
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOSI6bPYcKKMM2oZyN6dvl8YnYgxlj0jU-QHzAnB853V0BKZEpBhWELAZ5iidsiJmPdBI&usqp=CAU"
+              alt="child"
+              style={{ height: "200px" }}
+            />
+            <div class="card-body">
+              <h5 class="card-title">Blog title</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">
+                Blog subtitle
+              </h6>
+              <p class="card-text">Blog content along with author name</p>
+            </div>
+          </div>
+          <div
+            class="card"
+            style={{ width: "18rem", transition: "transform .9s" }}
+          >
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWTN7xFNTSp1eTUEIe2GMh1cwKABI1OqUA0wH6OyfY593hymMKnEslpXrxTykqJdVkcyk&usqp=CAU"
+              alt="child"
+              style={{ height: "200px" }}
+            />
+            <div class="card-body">
+              <h5 class="card-title">Blog title</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">
+                Blog subtitle
+              </h6>
+              <p class="card-text">Blog content along with author name</p>
+            </div>
+          </div>
+          <div
+            class="card"
+            style={{
+              width: "18rem",
+              transition: "transform .9s",
+              marginRight: "15px",
+            }}
+          >
+            <img
+              src="https://www.himama.com/blog/wp-content/uploads/2018/12/header_mom-and-daughter-with-tablet.jpg"
+              alt="child"
+              style={{ height: "200px" }}
+            />
+            <div class="card-body">
+              <h5 class="card-title">Blog title</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary">
+                Blog subtitle
+              </h6>
+              <p class="card-text">Blog content along with author name</p>
+            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Freeb
+export default Freeb;
--- src/pages/commonHomePage/Components/Goals.js
+++ src/pages/commonHomePage/Components/Goals.js
@@ -1,38 +1,42 @@
-import React from 'react'
-import parenting4 from '../Assets/parenting4.jpg'
-import parenting5 from '../Assets/parenting5.jpg'
-import parenting6 from'../Assets/parenting6.jpg'
-import './Goals.css'
+import React from "react";
+import parenting4 from "../Assets/parenting4.jpg";
+import parenting5 from "../Assets/parenting5.jpg";
+import parenting6 from "../Assets/parenting6.jpg";
+import "./Goals.css";
 function Goals() {
   return (
     <>
-      <div className='goals'>
-        <p><b>OUR GOALS</b></p>
+      <div className="goals">
+        <p className="mt-5 ms-3">
+          <b>OUR GOALS</b>
+        </p>
       </div>
-      <div className='goal-container'>
-        <div className='goal-row'>
+      <div className="goal-container">
+        <div className="goal-row">
+          <div className="goal1">
+            <div className="h-50">
+              <img src={parenting4} alt="Parenting" />
+            </div>
 
-          <div className='goal1'>
-            <img src={parenting4} alt="Parenting" />
-            
-           <h2>Revloutionary</h2>
-           <p>Parenting and Caregiving done through a new way. </p>
+            <h2 className="mt-3">Revloutionary</h2>
+            <p>Parenting and Caregiving done through a new way. </p>
           </div>
-          <div className='goal2'>
-            <img src={parenting5} alt="Parenting" />
-            <h2>Interactive</h2>
-           <p>Mold your childs behaviour through interactive activities.</p>
+          <div className="goal2">
+            <div className="h-50">
+              <img src={parenting5} alt="Parenting" />
+            </div>
+            <h2 className="mt-3">Interactive</h2>
+            <p>Mold your childs behaviour through activities.</p>
           </div>
-          <div className='goal3'>
+          <div className="goal3">
             <img src={parenting6} alt="Parenting" />
-            <h2>Expert Opinions</h2>
-           <p>Our community is here to support you in every steps.</p>
+            <h2 className="mt-3">Expert Opinions</h2>
+            <p>Our community is here to support you.</p>
           </div>
         </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default Goals
+export default Goals;
