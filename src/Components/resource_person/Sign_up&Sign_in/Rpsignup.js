af79500d28417d15881efaf7f9b19d94108e73e7
Anand R P <anandrp.sics@gmail.com>
2024-03-02 00:59:53 +0530

    rp registration requests acceipt and reject by admin

--- src/Components/resource_person/Sign_up&Sign_in/Rpsignup.js
+++ src/Components/resource_person/Sign_up&Sign_in/Rpsignup.js
@@ -1,234 +1,320 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Icon } from '@iconify-icon/react'
-import "./Rpsignup.css"
-import axios from "axios"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify-icon/react";
+import "./Rpsignup.css";
+import axios from "axios";
+import axiosInstance from "../../../apis/axiosInstance";
 
 function Rpsignup() {
+  const [rpsignup, setRpsignup] = useState({
+    name: "rp name",
+    age: "20",
+    email: "rp@gmail.com",
+    contact: "1234597890",
+    qualification: "No formal education",
+    experienceYear: "20",
+    password: "12341234",
+    img: null,
+    profilePicture: "",
+  });
 
-    const [rpsignup, setRpsignup] = useState({
+  const [errors, setErrors] = useState({
+    name: "",
+    age: "",
+    email: "",
+    contact: "",
+    qualification: "",
+    experienceYear: "",
+    password: "",
+    img: "",
+    profilePicture: "",
+  });
 
-        name: "",
-        age: "",
-        email: "",
-        contact: "",
-        qualification: "",
-        experienceYear: "",
-        password: "",
-        certificateImg: "",
-        profilePicture: ""
+  const handleFileChange = (e) => {
+    setRpsignup({ ...rpsignup, [e.target.name]: e.target.files[0] });
 
-    })
+    setErrors((preErrors) => ({ ...preErrors, [e.target.name]: "" }));
+  };
 
-    const [errors, setErrors] = useState({
+  const changefn = (e) => {
+    const { name, value } = e.target;
 
-        name: "",
-        age: "",
-        email: "",
-        contact: "",
-        qualification: "",
-        experienceYear: "",
-        password: "",
-        certificateImg: "",
-        profilePicture: ""
+    setRpsignup((preData) => ({ ...preData, [name]: value }));
 
-    })
+    setErrors((preErrors) => ({ ...preErrors, [name]: "" }));
+  };
 
-    const changefn = (e) => {
-        const { name, value } = e.target
-        setRpsignup(preData => ({ ...preData, [name]: value }))
-        setErrors(preErrors => ({ ...preErrors, [name]: "" }))
+  let formValid = true;
+
+  const submitfn = (e) => {
+    e.preventDefault();
+
+    let errors = {};
+
+    if (!rpsignup.name.trim()) {
+      formValid = false;
+      errors.name = "Name is required";
     }
 
-    let formValid = true
-
-    const submitfn = (e) => {
-        e.preventDefault();
-
-        let errors = {}
-
-        if (!rpsignup.name.trim()) {
-            formValid = false
-            errors.name = "Name is required"
-        }
-
-        if (!rpsignup.age.trim()) {
-            formValid = false
-            errors.age = "Age is required"
-        }
-
-        if (!rpsignup.email.trim()) {
-            formValid = false
-            errors.email = "Email is required"
-        }
-
-        if (!rpsignup.contact.trim()) {
-            formValid = false
-            errors.contact = "Contact number is required"
-        }
-        else if (rpsignup.contact.length < 10) {
-            errors.contact = "Enter valid number"
-        }
-
-        if (!rpsignup.qualification.trim()) {
-            formValid = false
-            errors.qualification = "Qualification field is required"
-        }
-
-        if (!rpsignup.experienceYear.trim()) {
-            formValid = false
-            errors.experienceYear = "Experience field is required"
-        }
-
-        if (!rpsignup.password.trim()) {
-            formValid = false
-            errors.password = "Password is required"
-        }
-        else if (rpsignup.password.length < 5) {
-            errors.password = "Password should be atleast 6 characters"
-        }
-
-        if (!rpsignup.certificateImg.trim()) {
-            formValid = false
-            errors.certificateImg = "No files choosen"
-        }
-
-        if (!rpsignup.profilePicture.trim()) {
-            formValid = false
-            errors.profilePicture = "No files choosen"
-        }
-
-        setErrors(errors)
-
-        if (formValid) {
-
-            axios.post("http://localhost:4009/smart_parent/registerRp", rpsignup, {
-                headers: {
-                    "Content-Type": "multipart/form-data",
-                },
-            })
-                .then((res) => { console.log("response", res); alert("Form is submitted") })
-                .catch((err) => { console.log("error", err);
-                alert(err.response.data.message)
-            
-            })
-
-        }
-        else { console.log("form", formValid) }
-
+    if (!rpsignup.age.trim()) {
+      formValid = false;
+      errors.age = "Age is required";
     }
-    return (
-        <div className='signup'>
 
-            <h1>Resource Person Sign up</h1>
+    if (!rpsignup.email.trim()) {
+      formValid = false;
+      errors.email = "Email is required";
+    }
 
-            <div className='regform'>
+    if (!rpsignup.contact.trim()) {
+      formValid = false;
+      errors.contact = "Contact number is required";
+    } else if (rpsignup.contact.length < 10) {
+      errors.contact = "Enter 10 digits valid number";
+    }
 
-                <form onSubmit={submitfn} >
+    if (!rpsignup.qualification.trim()) {
+      formValid = false;
+      errors.qualification = "Qualification field is required";
+    }
 
-                    <div className="input-box">
-                        <div className='label'> <label>Name</label>  </div>
-                        <input type="text" placeholder="Name" name="name" onChange={changefn} />
+    if (!rpsignup.experienceYear.trim()) {
+      formValid = false;
+      errors.experienceYear = "Experience field is required";
+    }
 
-                        {errors.name && <div className="text-danger errortext">{errors.name}</div>}
+    if (!rpsignup.password.trim()) {
+      formValid = false;
+      errors.password = "Password is required";
+    } else if (rpsignup.password.length < 5) {
+      errors.password = "Password should be atleast 6 characters";
+    }
 
-                    </div>
+    setErrors(errors);
 
+    if (
+      rpsignup.name &&
+      rpsignup.age &&
+      rpsignup.email &&
+      rpsignup.contact &&
+      rpsignup.qualification &&
+      rpsignup.experienceYear &&
+      rpsignup.password
+    ) {
+      formValid = true;
+    }
+    if (formValid) {
+      sendDataToServer();
+    } else {
+      console.log("form is not valid", formValid);
+      console.log("data entered", rpsignup);
+    }
 
-                    <div className="input-box">
-                        <div className='label'> <label>Age</label>  </div>
-                        <input type="text" placeholder="Age" name="age" onChange={changefn} />
+    function sendDataToServer() {
+      axiosInstance
+        .post("smart_parent/registerRp", rpsignup)
+        .then((res) => {
+          console.log("response", res);
+          alert("Waiting for Admin approval..");
+        })
+        .catch((err) => {
+          console.log("error", err);
+          let msg = err?.response?.data?.message || "Error occured";
+          alert(msg);
+        });
+    }
+  };
 
-                        {errors.age && <div className="text-danger errortext">{errors.age}</div>}
+  return (
+    <div className="signup">
+      <h1>Resource Person Sign up</h1>
 
-                    </div>
-
-
-                    <div className="input-box">
-                        <div className='label'> <label>Email</label>  </div>
-                        <input type="email" placeholder="Email" name="email" onChange={changefn} />
-
-                        {errors.email && <div className="text-danger errortext">{errors.email}</div>}
-
-                    </div>
-
-
-                    <div className="input-box">
-                        <div className='label'> <label>Contact</label>  </div>
-                        <input type="text" placeholder="Contact Number" name="contact" onChange={changefn} />
-
-                        {errors.contact && <div className="text-danger errortext">{errors.contact}</div>}
-
-                    </div>
-
-
-                    <div className='input-box'>
-                        <div className='label'> <label>Qualification</label>  </div>
-                        <select name='qualification' onChange={changefn}>
-                            <option selected>Select One</option>
-                            <option value="No formal education" >No formal education</option>
-                            <option value="Primary education" >Primary education</option>
-                            <option value="Secondary education or high school" >Secondary education or high school</option>
-                            <option value="Vocational qualification" >Vocational qualification</option>
-                            <option value="Bachelor's degree" >Bachelor's degree</option>
-                            <option value="Master's degree" >Master's degree</option>
-                            <option value="Doctorate or higher" >Doctorate or higher</option>
-                        </select>
-
-                        {errors.qualification && <div className="text-danger errortext">{errors.qualification}</div>}
-
-                    </div>
-
-
-                    <div className="input-box">
-                        <div className='label'> <label>Experience</label>  </div>
-                        <input type="text" placeholder="Experience" name="experienceYear" onChange={changefn} />
-
-                        {errors.experienceYear && <div className="text-danger errortext">{errors.experienceYear}</div>}
-
-                    </div>
-
-
-                    <div className="input-box">
-                        <div className='label'> <label>Password</label>  </div>
-                        <input type="password" placeholder="Password" name="password" onChange={changefn} />
-
-                        {errors.password && <div className="text-danger errortext">{errors.password}</div>}
-
-                    </div>
-
-
-                    <div className='files' >
-                        <div className='label'> <label>Certificate</label>  </div>
-                        <input type='file' name="certificateImg" onChange={changefn} />
-
-                        {errors.certificateImg && <div className="text-danger errortext">{errors.certificateImg}</div>}
-
-                    </div>
-
-
-                    <div className='files'>
-                        <div className='label'> <label>Profile Picture</label>  </div>
-                        <input type='file' name="profilePicture" onChange={changefn} />
-
-                        {errors.profilePicture && <div className="text-danger errortext">{errors.profilePicture}</div>}
-
-                    </div>
-
-                    <div className="text">
-                        <h5>Already have an account? <Link to="">Login now</Link></h5>
-                    </div>
-
-                    <div className="inbutton">
-                        <button type="submit">Sign up  <Icon icon="grommet-icons:connect" className='icon' /></button>
-                    </div>
-
-                </form>
-
+      <div className="regform">
+        <form onSubmit={submitfn}>
+          <div className="input-box">
+            <div className="label">
+              {" "}
+              <label>Name</label>{" "}
             </div>
+            <input
+              type="text"
+              placeholder="Name"
+              value={rpsignup.name}
+              name="name"
+              onChange={changefn}
+            />
 
-        </div>
-    )
+            {errors.name && (
+              <div className="text-danger errortext">{errors.name}</div>
+            )}
+          </div>
+
+          <div className="input-box">
+            <div className="label">
+              {" "}
+              <label>Age</label>{" "}
+            </div>
+            <input
+              type="text"
+              placeholder="Age"
+              name="age"
+              value={rpsignup.age}
+              onChange={changefn}
+            />
+
+            {errors.age && (
+              <div className="text-danger errortext">{errors.age}</div>
+            )}
+          </div>
+
+          <div className="input-box">
+            <div className="label">
+              {" "}
+              <label>Email</label>{" "}
+            </div>
+            <input
+              type="email"
+              value={rpsignup.email}
+              placeholder="Email"
+              name="email"
+              onChange={changefn}
+            />
+
+            {errors.email && (
+              <div className="text-danger errortext">{errors.email}</div>
+            )}
+          </div>
+
+          <div className="input-box">
+            <div className="label">
+              {" "}
+              <label>Contact</label>{" "}
+            </div>
+            <input
+              type="text"
+              value={rpsignup.contact}
+              placeholder="Contact Number"
+              name="contact"
+              onChange={changefn}
+            />
+
+            {errors.contact && (
+              <div className="text-danger errortext">{errors.contact}</div>
+            )}
+          </div>
+
+          <div className="input-box">
+            <div className="label">
+              {" "}
+              <label>Qualification</label>{" "}
+            </div>
+            <select
+              name="qualification"
+              onChange={changefn}
+              value={rpsignup.qualification}
+            >
+              <option selected>Select One</option>
+              <option value="No formal education">No formal education</option>
+              <option value="Primary education">Primary education</option>
+              <option value="Secondary education or high school">
+                Secondary education or high school
+              </option>
+              <option value="Vocational qualification">
+                Vocational qualification
+              </option>
+              <option value="Bachelor's degree">Bachelor's degree</option>
+              <option value="Master's degree">Master's degree</option>
+              <option value="Doctorate or higher">Doctorate or higher</option>
+            </select>
+
+            {errors.qualification && (
+              <div className="text-danger errortext">
+                {errors.qualification}
+              </div>
+            )}
+          </div>
+
+          <div className="input-box">
+            <div className="label">
+              {" "}
+              <label>Experience</label>{" "}
+            </div>
+            <input
+              type="text"
+              placeholder="Experience"
+              name="experienceYear"
+              value={rpsignup.experienceYear}
+              onChange={changefn}
+            />
+
+            {errors.experienceYear && (
+              <div className="text-danger errortext">
+                {errors.experienceYear}
+              </div>
+            )}
+          </div>
+
+          <div className="input-box">
+            <div className="label">
+              {" "}
+              <label>Password</label>{" "}
+            </div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={rpsignup.password}
+              name="password"
+              onChange={changefn}
+            />
+
+            {errors.password && (
+              <div className="text-danger errortext">{errors.password}</div>
+            )}
+          </div>
+
+          {/* <div className="files">
+            <div className="label">
+              {" "}
+              <label>Certificate</label>{" "}
+            </div>
+            <input type="file" name="img" accept="image/*" onChange={handleFileChange} />
+
+            {errors.certificateImg && (
+              <div className="text-danger errortext">
+                {errors.img}
+              </div>
+            )}
+          </div> */}
+
+          {/* <div className="files">
+            <div className="label">
+              {" "}
+              <label>Profile Picture</label>{" "}
+            </div>
+            <input type="file" name="profilePicture" onChange={changefn} />
+
+            {errors.profilePicture && (
+              <div className="text-danger errortext">
+                {errors.profilePicture}
+              </div>
+            )}
+          </div> */}
+
+          <div className="text">
+            <h5>
+              Already have an account? <Link to="">Login now</Link>
+            </h5>
+          </div>
+
+          <div className="inbutton">
+            <button type="submit">
+              Sign up <Icon icon="grommet-icons:connect" className="icon" />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Rpsignup
+export default Rpsignup;
--- src/Components/user/pages/Admin/RPLIst.js
+++ src/Components/user/pages/Admin/RPLIst.js
@@ -1,43 +1,135 @@
 import React from "react";
 import "./rplist.css";
 import Sidebar from "./Sidebar";
+import axiosInstance from "../../../../apis/axiosInstance";
+import { useState, useEffect } from "react";
 
 function RPLIst() {
+  const [rpLists, setRpLists] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+  function getData() {
+    axiosInstance
+      .get("smart_parent/view-all-rp")
+      .then((res) => {
+        console.log("res", res);
+        let allRps = res?.data?.data || [];
+        const filterPendingReqs = allRps.filter(
+          (rp) => rp?.isAdminApproved == "pending"
+        );
+
+        setRpLists(filterPendingReqs);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
+  function handleRejectClick(id) {
+    console.log(id);
+    axiosInstance
+      .post("smart_parent/rejectRpRegistration/" + id)
+      .then((res) => {
+        console.log("res", res);
+        if (res.status === 200) {
+          let msg =
+            res?.data?.message ||
+            "Resource Person Registration Request Rejected";
+          alert(msg);
+          getData();
+        } else {
+          console.log("err on reject request");
+        }
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
+
+  function handleAcceptClick(id) {
+    console.log("id", id);
+    axiosInstance
+      .post("smart_parent/acceptRpRegistration/" + id)
+      .then((res) => {
+        console.log("res", res);
+        if (res.status === 200) {
+          let msg =
+            res?.data?.message ||
+            "Resource Person Registration Request Accepted";
+          alert(msg);
+          getData();
+        } else {
+          console.log("err on accept request");
+        }
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
   return (
     <div>
       <div className="row">
         <div className="col-2">
-        <Sidebar/>
+          <Sidebar />
         </div>
-        <div className="col-8 ms-5 container">
-        <h3 className="mt-5">All Resource person Requests</h3>
-        <table class="table">
-        <thead>
-          <tr>
-          <th scope="col">profilePicture</th>
-            <th scope="col">name</th>
-            <th scope="col">qualification</th>
-            <th scope="col">experienceYear</th>
-            <th scope="col">age</th>
-            <th scope="col">Email</th>
-            <th scope="col">Accept</th>
-            <th scope="col">Reject</th>
-
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mar  jjjjjjjjjjk</td>
-            <td>Otmjjjjjjjjjto</td>
-            <td>@mmmmmmmmmmmmmmmmdo</td>
-            <td>Mardmvnjfvhk</td>
-            <td>Otkdvnmkjjjjjjjjjjjjjjjjjjjjjjjjjjfvfto</td>
-            <td><button>Accept</button></td>
-            <td><button>Reject</button></td>
-          </tr>
-        </tbody>
-      </table>
+        <div style={{ maxWidth: "77%" }} className="container">
+          {rpLists.length === 0 && (
+            <h1 className="mt-5"> No Resource Person Requests Found</h1>
+          )}
+          {rpLists.length > 0 && (
+            <div>
+              <h3 className="mt-5">All Resource person Requests</h3>
+              <table style={{ width: "100%" }}>
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Experience Year</th>
+                    <th>Age</th>
+                    <th>Phone Number</th>
+                    <th>Accept</th>
+                    <th>Reject</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {rpLists.map((rp, index) => {
+                    return (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{rp.name}</td>
+                        <td>{rp.email}</td>
+                        <td>{rp.experienceYear}</td>
+                        <td>{rp.age}</td>
+                        <td>{rp.contact}</td>
+                        <td>
+                          <button
+                            className="btn btn-success rp-request-handls-btn"
+                            onClick={() => {
+                              handleAcceptClick(rp._id);
+                            }}
+                          >
+                            Accept
+                          </button>
+                        </td>
+                        <td>
+                          <button
+                            className="btn btn-danger rp-request-handls-btn"
+                            onClick={() => {
+                              handleRejectClick(rp._id);
+                            }}
+                          >
+                            Reject
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
       </div>
     </div>
