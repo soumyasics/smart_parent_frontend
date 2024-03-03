c6a749b6c28fd6bb6ab566e1afc907763cab7be9
Anand R P <anandrp.sics@gmail.com>
2024-03-03 20:44:20 +0530

    resouce person view videos page

--- src/pages/resouce-person/rp-view-tutorial/rp-view-tutorial.jsx
+++ src/pages/resouce-person/rp-view-tutorial/rp-view-tutorial.jsx
@@ -1,14 +1,43 @@
 import Footer from "../../commonHomePage/Components/commonFooter";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 const RpViewTutorials = () => {
-    return (
-        <>
-        <h1> Navbar Here </h1>
-            <div>
+  return (
+    <>
+      <h1> Navbar Here </h1>
+      <div>
+        <h1 className="text-center"> Tutorials </h1>
+        <div
+          style={{ width: "90%", minHeight: "500px" }}
+          className="bg-primary video-tutorials mx-auto d-flex flex-wrap p-4 gap-3"
+        >
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(() => {
+            return (
+              <Card style={{ width: "18rem", maxHeight: "400px" }}>
+                <Card.Img
+                  style={{ maxHeight: "50%" }}
+                  className="h-50"
+                  variant="top"
+                  src="https://media.istockphoto.com/id/1275089443/video/children-playing-with-funny-animals.jpg?s=640x640&k=20&c=o1zD8-WEbHt4XdpWHMPEpTa089MenK-wk6U7qV3JKNQ="
+                />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </div>
+      </div>
+      <div className="mt-5">
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-
-            </div>
-        </>
-    )
-}
-
-export default RpViewTutorials
+export default RpViewTutorials;
