b6fc8c16a8d75024441f1a40b96a6a727c82ad87
adarshajith24 <adarshajithp.sics@gmail.com>
2024-03-05 22:03:27 -0800

    send

--- src/Components/resource_person/RP_Pages/Blog/Rpblog.js
+++ src/Components/resource_person/RP_Pages/Blog/Rpblog.js
@@ -4,83 +4,83 @@ import Rpnav from '../../navbar/Rpnav'
 import Footer from '../../../../pages/commonHomePage/Components/commonFooter'
 
 function Rpblog() {
-  return (
+    return (
 
-    <>
-    <Rpnav/>
+        <>
+            <Rpnav />
 
-    <div className='Rpblog'>
+            <div className='Rpblog'>
 
-<div className='rpblogheading'>
-<h1>BLOG POST</h1>
-</div>
-
-
-<div className='rpblogform'>
+                <div className='rpblogheading'>
+                    <h1>BLOG POST</h1>
+                </div>
 
 
-<div className='rpblogfield'>
-<div className='rpbloginputs'>
-    <div className='bloglabel'>
-        <label>Title</label>
-    </div>
-    <input type='text' />
-
-</div>
-</div>
-
-<div className='rpblogfield'>
-<div className='rpblogarea'>
-    <div className='bloglabel'>
-        <label>Blog Content</label>
-    </div>
-    <textarea/>
-
-    </div>
-</div>
-
-<div className='rpblogfield'>
-
-<div className='rpblogarea'>
-    <div className='bloglabel'>
-        <label>Conclusion</label>
-    </div>
-    <textarea/>
-    </div>
-</div>
-
-<div className='rpblogfield'>
-<div className='blogfiles'>
-    <div className='bloglabel'>
-        <label>Images</label>
-    </div>
-    
-    <div className='rpimg1'>
-    <input type='file'/>
-    </div>
-
-    <div className='rpimg2'>
-
-    <input type='file'/>
-
-    </div>
-  
-    </div>
-
-</div>
-  
-<div className='blogbutton'>
-    <button>Submit</button>
-</div>
+                <div className='rpblogform'>
 
 
-</div>
-    </div>
-    <Footer/>
-    </>
+                    <div className='rpblogfield'>
+                        <div className='rpbloginputs'>
+                            <div className='bloglabel'>
+                                <label>Title</label>
+                            </div>
+                            <input type='text' />
 
-  
-  )
+                        </div>
+                    </div>
+
+                    <div className='rpblogfield'>
+                        <div className='rpblogarea'>
+                            <div className='bloglabel'>
+                                <label>Blog Content</label>
+                            </div>
+                            <textarea />
+
+                        </div>
+                    </div>
+
+                    <div className='rpblogfield'>
+
+                        <div className='rpblogarea'>
+                            <div className='bloglabel'>
+                                <label>Conclusion</label>
+                            </div>
+                            <textarea />
+                        </div>
+                    </div>
+
+                    <div className='rpblogfield'>
+                        <div className='blogfiles'>
+                            <div className='bloglabel'>
+                                <label>Images</label>
+                            </div>
+
+                            <div className='rpimg1'>
+                                <input type='file' />
+                            </div>
+
+                            <div className='rpimg2'>
+
+                                <input type='file' />
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className='blogbutton'>
+                        <button>Submit</button>
+                    </div>
+
+
+                </div>
+            </div>
+            <Footer />
+        </>
+
+
+    )
 }
 
 export default Rpblog
