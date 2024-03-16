9f4d0f28d5a238195723b5599b318a47fc879f02
Anand R P <anandrp.sics@gmail.com>
2024-03-16 15:03:35 +0530

    user search functionality implemented on chat page

--- src/Components/user/User_Pages/rpchat/Userchat.js
+++ src/Components/user/User_Pages/rpchat/Userchat.js
@@ -8,12 +8,41 @@ import Userinnerchat from "./Userinnerchat";
 import { Link } from "react-router-dom";
 
 function Userchat({ onSelectRecipient, subscribers }) {
+  const [allSubs, setAllSubs] = useState([]);
+  const [varySubs, setVarySubs] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setAllSubs(subscribers);
+    setVarySubs(subscribers);
+  }, [subscribers]);
+
+  function filterSearchedItem(e) {
+    console.log(e.key, "ek");
+    if (e.key === "Enter") {
+      if (!search) {
+        setVarySubs(allSubs);
+        return;
+      }
+
+      const filtedSubs = allSubs.filter((e) => {
+        return e.parentId?.name.toLowerCase().includes(search.toLowerCase());
+      });
+
+      setVarySubs(filtedSubs);
+    }
+  }
   return (
     <div className="row mt-2">
       <div className="chatsearch ">
         <input
           style={{ width: "95%" }}
           type="text"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          onKeyDown={filterSearchedItem}
           placeholder="Search Parents"
           className="search_icon_input ms-3"
         />
@@ -23,16 +52,15 @@ function Userchat({ onSelectRecipient, subscribers }) {
         style={{ height: "600px" }}
       >
         <div className="friendsimg">
-          {subscribers?.map((parent, index) => {
+          {varySubs?.map((parent, index) => {
             let name = parent?.parentId?.name || "";
             let mail = parent?.parentId?.email || "";
             let parentId = parent?.parentId?._id || null;
             let rpProfilePicture = chatimg;
-            
+
             return (
               <div
                 className="chat-persons list_view mb-3"
-                
                 style={{
                   cursor: "pointer",
                 }}
